1cbf65f4d60427e7134f6630a1a38b536f626de6
siemensikkema <siemensikkema@users.noreply.github.com>
2021-09-27 12:05:05 +0200

    Remove init

--- Sources/Validations/KeyedError.swift
+++ Sources/Validations/KeyedError.swift
@@ -1,11 +1,6 @@
 import Decoded
 
 public struct KeyedError {
-    public init(codingPath: CodingPath, error: Error) {
-        self.codingPath = codingPath
-        self.error = error
-    }
-
     let codingPath: CodingPath
     let error: Error
 }
