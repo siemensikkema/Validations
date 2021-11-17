22d4f920ccee3b1ccf56e0ae695c1d46d2b6b38a
siemensikkema <siemensikkema@users.noreply.github.com>
2021-11-17 13:55:09 +0100

    Add KeyedFailures tests and docs

--- Sources/Validations/KeyedFailures.swift
+++ Sources/Validations/KeyedFailures.swift
@@ -1,6 +1,8 @@
 import Decoded
 
+/// Multiple values keyed by `CodingPath`.
 public struct KeyedValues<T> {
+    /// The dictionary underlying the `KeyedValues` instance.
     public let value: [CodingPath: [T]]
 
     private init(_ value: [CodingPath: [T]]) {
@@ -8,7 +10,57 @@ public struct KeyedValues<T> {
     }
 }
 
+extension KeyedValues: Encodable where T: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(
+            Dictionary(
+                uniqueKeysWithValues: value
+                    .map { codingPath, values in
+                        (codingPath.dotPath, values)
+                    }
+            )
+        )
+    }
+}
+
 /// Represents an unsuccessful validation with one or more ``ValidationFailure`` values per `CodingPath`.
+///
+/// ``KeyedFailures`` provides APIs for making the validation failures presentable (see ``KeyedValues``):
+/// - `mapFailures(_:)`
+/// - `mapFailuresWithCodingPath(_:)`
+///
+/// As an example, suppose you want to return an error response with error codes for certain failures. You could then define a protocol and conform any ``ValidationFailure`` you want to be associated with an error code:
+///
+/// ```swift
+/// protocol CodedError {
+///     var errorCode: Int { get }
+/// }
+/// ```
+///
+/// A response to represent your error:
+///
+/// ```swift
+/// struct ErrorResponse: Encodable {
+///     let code: Int?
+///     let description
+///     init(failure: ValidationFailure) {
+///         self.code = (failure as? CodedError)?.errorCode
+///         self.description = String(describing: failure)
+///     }
+/// }
+/// ```
+///
+/// And finally, catch the error and map the failures:
+///
+/// ```swift
+/// do {
+///     let validated = try decoded.validated()
+/// } catch let failures as KeyedFailures {
+///     let keyedErrorResponses = failures.mapFailures(ErrorResponse.init)
+///     ...
+/// }
+/// ```
 public typealias KeyedFailures = KeyedValues<ValidationFailure>
 
 extension KeyedFailures: Error {}
@@ -23,16 +75,36 @@ extension KeyedFailures {
         self = .init(value.merging(other.value, uniquingKeysWith: +))
     }
 
+    /// Transforms the validation failures into new types.
+    ///
+    /// - Parameter transform: Closure that transforms each `ValidationFailure` into a new type.
+    /// - Returns: A `KeyedValues` containing the same keys but with transformed values.
     public func mapFailures<U>(_ transform: (T) -> U) -> KeyedValues<U> {
         .init(value.mapValuesEach(transform))
     }
+
+    /// Transforms the validation failures into new types.
+    ///
+    /// - Parameter transform: Closure that takes a `CodingPath` and `ValidationFailure` and transforms each failure into a new type.
+    /// - Returns: A `KeyedValues` containing the same keys but with transformed values.
+    public func mapFailuresWithCodingPath<U>(_ transform: (CodingPath, T) -> U) -> KeyedValues<U> {
+        .init(value.mapValuesEachWithKey(transform))
+    }
 }
 
-extension Dictionary where Value: Collection {
+extension Dictionary where Value: Sequence {
     func mapValuesEach<T>(_ transform: (Value.Element) -> T) -> [Key: [T]] {
-        mapValues { value in
-            value.map(transform)
+        mapValues { values in
+            values.map(transform)
         }
+    }
+
+    func mapValuesEachWithKey<T>(_ transform: (Key, Value.Element) -> T) -> [Key: [T]] {
+        .init(
+            uniqueKeysWithValues: map { key, values in
+                (key, values.map { transform(key, $0) })
+            }
+        )
     }
 }
 
@@ -53,4 +125,3 @@ extension KeyedFailures: KeyedFailuresRepresentable {
         .init(self)
     }
 }
-
---
+++ Tests/ValidationsTests/KeyedFailuresTests.swift
@@ -0,0 +1,63 @@
+import Decoded
+import XCTest
+import Validations
+
+final class KeyedFailuresTests: ValidationsTestCase {
+    func test_errorOutput() throws {
+        do {
+            let decoded: Decoded<Post> = try decode("""
+            {
+                "title": "My title",
+                "body": "invalid"
+            }
+            """)
+
+            // fail the validation on purpose
+            _ = try decoded.validated {
+                Validator(nestedAt: \.title, failure: Failure1())
+                Validator(nestedAt: \.body, failure: Failure2())
+            }
+        } catch let failures as KeyedFailures {
+            let errorOutput = failures.mapFailures(FailureWithCode.init)
+
+            // encode mapped failures and decode as a dictionary so we can compare with the expected outcome
+            let data = try JSONEncoder().encode(errorOutput)
+            let dict = try JSONDecoder().decode([String: [FailureWithCode]].self, from: data)
+            
+            XCTAssertEqual(dict, [
+                "title": [FailureWithCode(description: "failure1", code: 1)],
+                "body": [FailureWithCode(description: "failure2", code: nil)]
+            ])
+        }
+    }
+}
+
+fileprivate protocol CodedError {
+    var errorCode: Int { get }
+}
+
+fileprivate struct FailureWithCode: Codable, Equatable {
+    let description: String
+    let code: Int?
+}
+
+extension FailureWithCode {
+    init(failure: ValidationFailure) {
+        self.code = (failure as? CodedError)?.errorCode
+        self.description = String(describing: failure)
+    }
+}
+
+fileprivate struct Failure1: ValidationFailure, CodedError, CustomStringConvertible {
+    let description = "failure1"
+    let errorCode = 1
+}
+
+fileprivate struct Failure2: ValidationFailure, CustomStringConvertible {
+    let description = "failure2"
+}
+
+fileprivate struct Post: Decodable {
+    let title: Decoded<String>
+    let body: Decoded<String>
+}
